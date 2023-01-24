ff2ea0e53e729ddd8a722018af3319fb8cef729b
Gleison Mozart <mozart.gleison@gmail.com>
2023-01-23 22:40:48 -0300

    Finalizada a classe FuncionarioController

--- src/main/java/com/projetoselecaocefet/controller/FuncionarioController.java
+++ src/main/java/com/projetoselecaocefet/controller/FuncionarioController.java
@@ -5,10 +5,9 @@ import com.projetoselecaocefet.service.FuncionarioService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,6 +21,28 @@ public class FuncionarioController {
         funcionarioService.salvar(funcionarioDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FuncionarioDTO> consultaId(@PathVariable Long id){
+        return ResponseEntity.ok(funcionarioService.consultaId(id).orElseThrow());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<FuncionarioDTO> atualizar(@PathVariable Long id, @RequestBody FuncionarioDTO funcionarioDTO){
+        return ResponseEntity.ok(funcionarioService.atualizar(id, funcionarioDTO).orElseThrow());
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<FuncionarioDTO>> listarTodos(){
+        return ResponseEntity.ok(funcionarioService.listarTodos().orElseThrow());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id){
+        funcionarioService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 
 
 
