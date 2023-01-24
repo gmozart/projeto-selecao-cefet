0a28bc2d5d3bf0496471916628d99af3a3c7f7d5
gmozart <mozart.gleison@gmail.com>
2023-01-24 12:53:43 -0300

    Verificando erros de Build

--- src/main/java/com/projetoselecaocefet/repository/FuncionarioRepository.java
+++ src/main/java/com/projetoselecaocefet/repository/FuncionarioRepository.java
@@ -2,8 +2,15 @@ package com.projetoselecaocefet.repository;
 
 import com.projetoselecaocefet.entity.Funcionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
+
+
+
 }
--- src/test/java/com/projetoselecaocefet/service/FuncaoServiceTest.java
+++ src/test/java/com/projetoselecaocefet/service/FuncaoServiceTest.java
@@ -1,9 +1,7 @@
 package com.projetoselecaocefet.service;
 
-import com.projetoselecaocefet.dto.FuncaoDTO;
+
 import com.projetoselecaocefet.entity.Funcao;
-import com.projetoselecaocefet.entity.Salario;
-import com.projetoselecaocefet.enums.BeneficioEnum;
 import com.projetoselecaocefet.repository.FuncaoRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,9 +25,6 @@ class FuncaoServiceTest {
     @Test
     void whenSaveFuncaoTest() {
 
-          when(funcaoRepository.save(any())).thenReturn(Funcao.builder().salario
-           (Salario.builder().salario(10l).valorPorAnoServico(10l).build()).beneficio
-           (BeneficioEnum.toEnum(1)).build());
 
 
     }
